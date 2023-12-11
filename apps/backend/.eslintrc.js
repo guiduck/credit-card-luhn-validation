81820c3924a0fca23b6541587c7e6a06c1230564
guilherme <guilherme.figueiredo@cuponeria.com.br>
2023-12-11 13:41:34 -0300

    cleaning repo config

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -1,8 +1,39 @@
 /** @type {import("eslint").Linter.Config} */
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
 module.exports = {
-  extends: ["@repo/eslint-config/server.js"],
+  extends: [
+    "@vercel/style-guide/eslint/node",
+    "@vercel/style-guide/eslint/typescript",
+  ].map(require.resolve),
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  plugins: ["only-warn"],
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/**/*"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  ignorePatterns: ["node_modules/", "dist/"],
+  rules: {
+    "import/no-default-export": "off",
   },
 };
