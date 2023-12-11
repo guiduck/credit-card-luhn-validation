2da9cabcd7ec50be4a82b04b871d634f993e0e26
guilherme <guilherme.figueiredo@cuponeria.com.br>
2023-12-11 14:19:27 -0300

    env variables front

--- apps/frontend/src/services/api.ts
+++ apps/frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:5001/";
+
 export const api = axios.create({
-  baseURL: "http://localhost:5001/",
+  baseURL: baseUrl,
   headers: {
     "Content-Type": "application/json",
   },
